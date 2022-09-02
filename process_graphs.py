588399b7b90cd3bac4be371fe844ddaa260d6b0f
Ido Shamir <ido.shamir.ido@gmail.com>
2022-09-02 07:31:31 +0300

    pkl22

--- process_graphs.py
+++ process_graphs.py
@@ -2,7 +2,7 @@ import pandas as pd
 from os import listdir
 from os.path import isfile, join
 from os import rename
-from scipy.signal import find_peaks
+from scipy.signal import find_peaks,peak_prominences
 
 
 mypath = '.'
@@ -15,6 +15,24 @@ def isPeakAtEnd(df):
     found = [a for a in peaks if a in lastParts]
     return len(found) > 0
 
+def getPeakScore(df):
+    col = df.iloc[:, 0]
+    lastIndex = len(col)-1
+    lastParts = [lastIndex, lastIndex-1, lastIndex-2, lastIndex-3, lastIndex-4]
+    peaks, _ = find_peaks(col)
+    prominences = list(peak_prominences(col, peaks)[0])
+    peaksList = list(peaks)
+    maxLastParts = -1
+    for lastPart in lastParts:
+        i = 0
+        for curPeak in peaksList:
+            if curPeak == lastPart:
+                prominence = prominences[i]
+                if prominence > maxLastParts:
+                    maxLastParts = prominence
+            i += 1
+    return maxLastParts
+
 def getScore(df):
     col = df.iloc[:, 0]
     lastIndex = len(col)-1
@@ -25,14 +43,20 @@ def getScore(df):
             maxLastParts = col[i]
     return maxLastParts
 
-pklFiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f.endswith('.pkl')]
-for curFile in pklFiles:
-    curDf = pd.read_pickle(curFile)
-    score = getScore(curDf)
-    if isPeakAtEnd(curDf) and score >= 80:
-        rename(curFile, curFile + '2')
+#pklFiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f.endswith('.pkl')]
+#for curFile in pklFiles:
+#    curDf = pd.read_pickle(curFile)
+#    score = getScore(curDf)
+#    if isPeakAtEnd(curDf) and score >= 80:
+#        rename(curFile, curFile + '2')
 
 pkl2Files = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f.endswith('.pkl2')]
 for curFile in pkl2Files:
     curDf = pd.read_pickle(curFile)
+    score = getPeakScore(curDf)
+    if score >= 80:
+        rename(curFile, curFile + '2')
+
+pkl2Files = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f.endswith('.pkl22')]
+
 print(len(pkl2Files))
